6fb3653ff9431a4cd7cf8b3bd96b289833cfba6a
PKCpratyush <pratyush.choubey@unthinkable.co>
2022-01-14 18:17:56 +0530

    crud done

--- tackle_api/serializer.py
+++ tackle_api/serializer.py
@@ -2,9 +2,10 @@ from django.core.validators import validate_slug
 from rest_framework import serializers
 from .models import Student
 
+
 class Student_data_serializer(serializers.Serializer):
     roll_no = serializers.IntegerField()
-    name = serializers.CharField(max_length= 30)
+    name = serializers.CharField(max_length=30)
     city = serializers.CharField(max_length=30)
     # courses = serializers.CharField(max_length = 100)
 
@@ -12,11 +13,13 @@ class Student_data_serializer(serializers.Serializer):
         return Student.objects.create(**validate_data)
 
     def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.roll_no = validated_data.get('roll_no', instance.roll_no)
-        instance.city = validated_data.get('city', instance.city)
+        instance.name = validated_data.get("name", instance.name)
+        instance.roll_no = validated_data.get("roll_no", instance.roll_no)
+        instance.city = validated_data.get("city", instance.city)
         instance.save()
         return instance
 
+
+
 class Course_serializer(serializers.Serializer):
-    course = serializers.CharField(max_length = 100)
+    course = serializers.CharField(max_length=100)
--- tackle_api/views.py
+++ tackle_api/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from rest_framework import serializers
 from rest_framework.serializers import Serializer
 from rest_framework.utils import json
-from .models import Student,Courses
+from .models import Student, Courses
 from .serializer import Student_data_serializer, Course_serializer
 from rest_framework.renderers import JSONRenderer
 from django.http import HttpResponse, JsonResponse
@@ -20,19 +20,21 @@ from tackle_api import serializer
 
 # Create your views here.
 
+
 def student_detail(request, id=1):
-    student = Student.objects.get(roll_no = id)
+    student = Student.objects.get(roll_no=id)
     serializer = Student_data_serializer(student)
     course_list = ""
     subjects = Courses.objects.filter(student=student)
 
     json_data = JSONRenderer().render(serializer.data)
 
-    return HttpResponse(json_data, content_type = 'application/json')
+    return HttpResponse(json_data, content_type="application/json")
+
 
 def course_detail(request, id=1):
-    student = Student.objects.get(roll_no = id)
-    
+    student = Student.objects.get(roll_no=id)
+
     subjects = Courses.objects.filter(student=student)
     serializer = Course_serializer(subjects, many=True)
 
@@ -43,22 +45,24 @@ def course_detail(request, id=1):
     # safe=False for non dict objects otherwise left it as True as default
     return JsonResponse(serializer.data, safe=False)
 
+
 @csrf_exempt
 def insert_student_detail(request):
     if request.method == "POST":
         json_data = request.body
-        stream = io.BytesIO(json_data)# geeting stream object
+        stream = io.BytesIO(json_data)  # geeting stream object
         # converting into python data
         data = JSONParser().parse(stream)
-        serializer = Student_data_serializer(data = data)
+        serializer = Student_data_serializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            message = {"msg":"Data Inserted"}
+            message = {"msg": "Data Inserted"}
 
             return JsonResponse(message)
 
         else:
             return JsonResponse(JSONRenderer().render(serializer.errors))
+
 
 @csrf_exempt
 def update_student(request):
@@ -66,13 +70,13 @@ def update_student(request):
         json_data = request.body
         stream = io.BytesIO(json_data)
         data = JSONParser().parse(stream)
-        roll = data.get('roll_no')
+        roll = data.get("roll_no")
         # print(Student.objects.get(roll_no = roll).exist())
-        student_data = Student.objects.get(roll_no = roll)
+        student_data = Student.objects.get(roll_no=roll)
         # partial = True to allow to make partial update in the row
-        serializer = Student_data_serializer(student_data, data = data, partial=True)
+        serializer = Student_data_serializer(student_data, data=data, partial=True)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse({'msg':'Data Updated'})
+            return JsonResponse({"msg": "Data Updated"})
         else:
             return JsonResponse(JSONRenderer().render(serializer.errors))
